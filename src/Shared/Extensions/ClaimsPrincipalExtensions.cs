3d86235a1416631220347e3ee0fc0e1667d882f8
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2024-06-25 21:46:25 +0330

    Add extensions methods

---
+++ src/Shared/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace System.Security.Claims;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static T GetUserId<T>(this ClaimsPrincipal claimsPrincipal)
+    {
+        var userId = claimsPrincipal.GetUserId();
+        if (typeof(T) == typeof(Guid))
+        {
+            return (T)Convert.ChangeType(Guid.Parse(userId), typeof(T));
+        }
+        return (T)Convert.ChangeType(userId, typeof(T));
+    }
+
+    public static string? GetUserId(this ClaimsPrincipal claimsPrincipal)
+    {
+        return (claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier) ?? claimsPrincipal.FindFirst("nameid"))!.Value;
+    }
+
+    public static string GetUserName(this ClaimsPrincipal claimsPrincipal)
+    {
+        return (claimsPrincipal.FindFirst(ClaimTypes.Name) ?? claimsPrincipal.FindFirst("unique_name"))!.Value;
+    }
+
+    public static bool IsAuthenticated(this ClaimsPrincipal? claimsPrincipal)
+    {
+        return claimsPrincipal?.Identity?.IsAuthenticated is true;
+    }
+}
---
+++ src/Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace BytePlatform.Shared.Extensions;
+
+public static class StringExtensions
+{
+    public static string? TrimIfNotNull(this string? input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+        return input.Trim();
+    }
+
+    public static bool? IsNullOrEmpty(this string? input)
+        => string.IsNullOrEmpty(input);
+
+    public static bool? IsNullOrWhiteSpace(this string? input)
+        => string.IsNullOrWhiteSpace(input);
+
+    public static bool HasValue(this string? value, bool ignoreWhiteSpace = true)
+    {
+        return ignoreWhiteSpace
+            ? string.IsNullOrWhiteSpace(value) is false
+            : string.IsNullOrEmpty(value) is false;
+    }
+
+    public static bool HasNoValue(this string? value, bool ignoreWhiteSpace = true)
+    {
+        return ignoreWhiteSpace
+            ? string.IsNullOrWhiteSpace(value)
+            : string.IsNullOrEmpty(value);
+    }
+}
