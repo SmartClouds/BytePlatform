c5e09ec01ff62807e2d587a430b69db7e8a6f956
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2026-02-26 23:57:23 +0330

    Add LinqExtensions in Data project

---
+++ src/Server.Data/Extensions/LinqExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace BytePlatform.Server.Data.Extensions;
+
+public static class LinqExtensions
+{
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool predicate, Expression<Func<T, bool>> where)
+    {
+        if (predicate)
+            return query.Where(where);
+
+        return query;
+    }
+
+    public static IQueryable<T> SkipIf<T>(this IQueryable<T> query, bool predicate, int count)
+    {
+        return predicate ? query.Skip(count) : query;
+    }
+
+    public static IQueryable<T> TakeIf<T>(this IQueryable<T> query, bool predicate, int count)
+    {
+        return predicate ? query.Take(count) : query;
+    }
+}
